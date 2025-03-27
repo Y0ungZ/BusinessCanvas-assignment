993123b75c13e3477fcec8bf2d17ed5ba4c7e7f2
Y0ungZ <at8in@naver.com>
2025-03-28 06:20:01 +0900

    style: Pretendard 폰트 전역 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
+import './global.css';
+import { ConfigProvider } from 'antd';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          fontFamily: "'Pretendard', sans-serif",
+        },
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </StrictMode>,
 );
