9b986058fe80b4f94981d9b5a9234b3c4e0482e7
Y0ungZ <at8in@naver.com>
2025-03-29 05:42:46 +0900

    refactor: local-storage 적용

--- src/contexts/StorageProvider.tsx
+++ src/contexts/StorageProvider.tsx
@@ -1,10 +1,4 @@
-import {
-  createContext,
-  PropsWithChildren,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+import { createContext, PropsWithChildren, useContext, useState } from 'react';
 import { StorageType } from '../vite-env';
 import { UserDataType } from '../types/record';
 import { INITIAL_USER_RECORDS } from '../types/constants';
@@ -19,52 +13,98 @@ interface StorageState<T> {
   rowValueSet: (field: keyof T) => string[];
 }
 
-const StorageContext = createContext<StorageState<UserDataType> | undefined>(
-  undefined,
-);
+const StorageContext = createContext<StorageState<UserDataType> | null>(null);
 
 const StorageProvider = ({ children }: PropsWithChildren) => {
   const type: StorageType = import.meta.env.VITE_STORAGE || 'in-memory';
   const STORAGE_KEY = 'USERS';
+
+  const getLocalStorage = () => {
+    return localStorage.getItem(STORAGE_KEY);
+  };
+
+  const setLocalStorage = (records: UserDataType[]) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(records));
+  };
+
   const [records, setRecords] = useState<UserDataType[]>(() => {
     if (type === 'in-memory') {
       return [...INITIAL_USER_RECORDS];
     }
     if (type === 'local-storage') {
-      if (localStorage.getItem(STORAGE_KEY) === null) {
+      const localRecords = getLocalStorage();
+      if (localRecords === null) {
         return [...INITIAL_USER_RECORDS];
+      } else {
+        return JSON.parse(localRecords);
       }
     }
     return [];
   });
 
-  useEffect(() => {
-    if (type === 'local-storage') {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(records));
-    }
-  }, [records, type]);
-
   const createRecord = (record: UserDataType) => {
+    const normalizedRecord = Object.fromEntries(
+      Object.entries(record).map(([key, value]) => [
+        key,
+        value === undefined ? null : value,
+      ]),
+    );
+
     const day = dayjs(record.가입일).format('YYYY-MM-DD');
-    const newRecord = {
-      ...record,
+    const newRecord: UserDataType = {
+      ...normalizedRecord,
       id: nanoid(),
+      이름: record.이름,
       가입일: day,
+      '이메일 수신 동의': Boolean(record['이메일 수신 동의']),
     };
-    setRecords(prev => [...prev, newRecord]);
+
+    setRecords(prev => {
+      const createRecords = [...prev, newRecord];
+      if (type === 'local-storage') {
+        setLocalStorage(createRecords);
+      }
+      return createRecords;
+    });
   };
 
-  const updateRecord = (id: string, newRecord: UserDataType) => {
-    const day = dayjs(newRecord.가입일).format('YYYY-MM-DD');
-    const updateRecord = { ...newRecord, 가입일: day };
-    console.log(updateRecord);
-    setRecords(prev =>
-      prev.map(record => (record.id === id ? updateRecord : record)),
+  const updateRecord = (id: string, record: UserDataType) => {
+    const normalizedRecord = Object.fromEntries(
+      Object.entries(record).map(([key, value]) => [
+        key,
+        value === undefined ? null : value,
+      ]),
     );
+
+    const day = dayjs(record.가입일).format('YYYY-MM-DD');
+
+    const newRecord: UserDataType = {
+      ...normalizedRecord,
+      id: id,
+      이름: record.이름,
+      가입일: day,
+      '이메일 수신 동의': Boolean(record['이메일 수신 동의']),
+    };
+
+    setRecords(prev => {
+      const updateRecords = prev.map(record =>
+        record.id === id ? newRecord : record,
+      );
+      if (type === 'local-storage') {
+        setLocalStorage(updateRecords);
+      }
+      return updateRecords;
+    });
   };
 
   const deleteRecord = (id: string) => {
-    setRecords(prev => prev.filter(record => record.id !== id));
+    setRecords(prev => {
+      const deleteRecords = prev.filter(record => record.id !== id);
+      if (type === 'local-storage') {
+        setLocalStorage(deleteRecords);
+      }
+      return deleteRecords;
+    });
   };
 
   const rowValueSet = (field: keyof UserDataType) => {
